8593f2fe0df4db4196d1b3934da89f7678e545c5
juliafealves <juliafealves>
2018-02-03 20:37:10 -0300

    View improvements in CipherPage.

--- src/pages/cipher/cipher.ts
+++ src/pages/cipher/cipher.ts
@@ -1,32 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import {MusicPage} from "../music/music";
-
-/**
- * Generated class for the CipherPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { MusicPage } from '../music/music';
 
 @Component({
   selector: 'page-cipher',
   templateUrl: 'cipher.html',
 })
-export class CipherPage {
+export class CipherPage
+{
+  public ciphers = [];
 
-  ciphers = ['C', 'C#', 'D'];
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  ionViewDidLoad() {}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CipherPage');
-  }
-
-  searchMusic() {
-    this.navCtrl.push(MusicPage, {
-      ciphers: this.ciphers
-    });
+  /**
+   * Navigation between this page and MusicPage with ciphers chooses.
+   */
+  public searchMusic()
+  {
+    this.navCtrl.push(MusicPage, { ciphers: this.ciphers });
   }
 }
